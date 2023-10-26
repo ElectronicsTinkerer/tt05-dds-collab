6450e9f56244c4f2f41b6786ccbf72d40d9e20c6
Liam Crowley <62516277+LiamCrowley@users.noreply.github.com>
2023-10-26 12:42:01 -0400

    Update authorship

--- src/div14.v
+++ src/div14.v
@@ -1,23 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/23/2023 04:54:43 PM
-// Design Name: 
-// Module Name: div14
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/**
+Author: Liam Crowley
+Date: 10/23/2023 04:54:43 PM
+Clk divition for Phase Accumulator clk
+INPUTS: Clk
+OUTPUTS: Divided clk
+
+Divides main 50MHz clock by 18 for phase accumulator to give overhead for parallel to serial conversion
+*/
 
 ///DIVIDES 50MHz primary clock by 14 to give time for serialization w/ 2 clock cycles overhead
 //NEEDS DIV 18 NOT 14
