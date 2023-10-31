985da1763fb36504cbfa58b60642fd56cc97c7fe
LiamCrowley <liamcrowley5@gmail.com>
2023-10-31 11:10:51 -0400

    Fixed uio_oe being output instead of input.

--- src/tt_um_electronicstinkerer_dds_collab.v
+++ src/tt_um_electronicstinkerer_dds_collab.v
@@ -19,7 +19,7 @@ module tt_um_electronicstinkerer_dds_collab
     output wire [7:0] uo_out, // Dedicated outputs - connected to the 7 segment display
     input wire [7:0]  uio_in, // IOs: Bidirectional Input path
     output wire [7:0] uio_out, // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe, // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input wire [7:0] uio_oe, // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     input wire        ena, // will go high when the design is enabled
     input wire        clk, // clock
     input wire        rst_n     // reset_n - low to reset
