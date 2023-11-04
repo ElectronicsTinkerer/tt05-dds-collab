4e23cfc924dee1a942d51b9577571d3f9de9f72a
LiamCrowley <liamcrowley5@gmail.com>
2023-11-04 16:31:43 -0400

    Added mult testbench file

--- src/tb_Mult.v
+++ src/tb_Mult.v
@@ -11,14 +11,14 @@ Description   : Testbench for the multiplier module.
 */
 module tb_Mult (/*AUTOARG*/) ;
    reg   clk;
-   reg [11:0] a, b;
-   wire [23:0] M;
-   Mult #(.m(12)) UUT(.clk(clk),.a(a),.b(b),.mult(M));
+   reg [7:0] a, b;
+   wire [15:0] M;
+   Mult #(.m(8)) UUT(.clk(clk),.a(a),.b(b),.mult(M));
    initial begin
       clk = 0;
-      a = 12'haaa;
+      a = 8'haa;
 //12'b101;
-      b = 12'haaa;
+      b = 8'haa;
       forever #10 clk=~clk;
    end
    
